25b1621345b1da19106d266af8072f9885cef3be
frankgon1627 <fg1333762@gmail.com>
2025-03-07 00:13:25 -0500

    switching frames

--- positive_obstacle_detection/src/positive_obstacle_detection.cpp
+++ positive_obstacle_detection/src/positive_obstacle_detection.cpp
@@ -42,7 +42,7 @@ private:
 
     void transform_callback(){
         try{
-            lidar_to_map_transform_ = tf_buffer_.lookupTransform("map", "os_sensor", tf2::TimePointZero);
+            lidar_to_map_transform_ = tf_buffer_.lookupTransform("os_sensor", "map", tf2::TimePointZero);
         }
         catch (const tf2::TransformException &ex){
             RCLCPP_ERROR(this->get_logger(), "Failed to get transform: %s", ex.what());
